4229baf317461b6e35502cca20bb54dbc1b31add
lilithalicecrawford <lilithalicecrawford@gmail.com>
2020-05-06 13:12:56 -0500

    Add screen shader, cursor, splash, icons

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody
 
 # Declare member variables here.
-export(int) var SPEED : int = 100
-export(int) var MAX_SPEED : int  = 150
+export(int) var SPEED : int = 4
+export(int) var MAX_SPEED : int  = 8
 export(float) var LOOKAROUND_SPEED : float = 1
-export(float) var WEIGHT : float = 50
+export(float) var WEIGHT : float = 1
 
 var vel_y = 0
 var jumping = true
@@ -31,7 +31,7 @@ func _physics_process(delta : float):
 	clamped_origin.x = clamp(clamped_origin.x, -255, 255)
 	clamped_origin.z = clamp(clamped_origin.z, -255, 255)
 	set_transform(Transform(get_transform().basis, clamped_origin))
-	Global.set_debug_vector(clamped_origin)
+	# Global.set_debug_vector(clamped_origin)
 
 func movement(delta):
 	var speed : int = 0
@@ -55,20 +55,24 @@ func movement(delta):
 	_direction = direction
 	
 	# Move X- and Z- Axis
-	move_and_slide(direction * speed * delta)
+	move_and_slide(direction * speed)
 	
 	Global.clear_debug()
 	# handle collisions
 	for i in get_slide_count():
 		process_collision(get_slide_collision(i))
 	
+	if Input.is_action_just_pressed("move_jump") and not jumping:
+		vel_y = 0.05
+		jumping = true
 	
 	# apply gravity to new direction
 	vel_y -= WEIGHT / 400
 	direction = Vector3(0, vel_y*250, 0)
 	
 	# Move Y-Axis
-	move_and_slide(direction * delta)
+	move_and_slide(direction)
+	#vel_y = direction.y
 	
 	# handle collisions
 	for i in get_slide_count():
@@ -92,7 +96,6 @@ func clamp_rotation():
 		rot_x += PI*2
 
 func _input(event):
-	# Camera rotation
 	if event is InputEventMouseMotion: #and event.button_mask & 1:
 		# modify accumulated mouse rotation
 		rot_x -= event.relative.x * (LOOKAROUND_SPEED/100)
@@ -104,6 +107,6 @@ func _input(event):
 		$Camera.transform.basis = Basis()
 		$Camera.rotate_object_local(Vector3(1, 0, 0), rot_y)
 	# Jump!
-	elif not jumping and event is InputEventKey and event.get_scancode()==KEY_SPACE and event.is_pressed() and not event.is_echo():
-		vel_y = 3
-		jumping = true
+	#elif not jumping and event is InputEventKey and event.get_scancode()==KEY_SPACE and event.is_pressed() and not event.is_echo():
+		#vel_y = 0.05
+		#jumping = true
